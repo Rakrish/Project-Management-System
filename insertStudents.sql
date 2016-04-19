aff64b20e8dc9ffe160711d7b6f93daa32a50297
snehaKadham <sneha.kadham@yahoo.com>
2016-04-19 17:23:51 +0530

    Database inserts

---
+++ insertStudents.sql
@@ -0,0 +1,28 @@
+insert into StudentInfo values('1PI13CS162', 'Sneha Kadham', 'sneha162', 6);
+insert into StudEmail values('1PI13CS162', 'sneha.kadham@gmail.com');
+insert into StudContact values('1PI13CS162', '432156789');
+
+insert into StudentInfo values('1PI13CS160', 'Sirisha Vempati', 'sirisha160', 6);
+insert into StudEmail values('1PI13CS160', 'vempati.sirisha@gmail.com');
+insert into StudContact values('1PI13CS160', 563278914);
+
+insert into StudentInfo values('1PI13CS134', 'Sandesh', 'sandesh134', 4);
+insert into StudEmail values('1PI13CS134', 'sandesh@gmail.com');
+insert into StudContact values('1PI13CS134', 563728914);
+
+insert into StudentInfo values('1PI13CS110', 'Priyanka', 'priyanka110', 3);
+insert into StudEmail values('1PI13CS110', 'vkumarpriyanka@gmail.com');
+insert into StudContact values('1PI13CS110', 653278914);
+
+
+insert into StudentInfo values('1PI13CS140', 'Sharath S', 'sharath140', 6);
+insert into StudEmail values('1PI13CS140', 'sharath@gmail.com');
+insert into StudContact values('1PI13CS140', 563278941);
+
+insert into StudentInfo values('1PI13CS129', 'Rajath Krishna', 'rajath129', 5);
+insert into StudEmail values('1PI13CS129', 'rajathkrishna.sj@gmail.com');
+insert into StudContact values('1PI13CS129', 563287941);
+
+insert into StudentInfo values('1PI13CS146', 'Shilpa', 'shilpa146', 6);
+insert into StudEmail values('1PI13CS146', 'shilpa.naik8@gmail.com');
+insert into StudContact values('1PI13CS146', 563278491);
